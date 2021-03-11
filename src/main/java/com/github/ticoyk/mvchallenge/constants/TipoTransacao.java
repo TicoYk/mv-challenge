5786189a58a98e1b6955d8b42a2c268423f97130
TicoYk <didimoykym@gmail.com>
2021-03-11 18:16:00 -0300

    Adicionado atributos da Transação. #4

---
+++ src/main/java/com/github/ticoyk/mvchallenge/constants/TipoTransacao.java
@@ -0,0 +1,5 @@
+package com.github.ticoyk.mvchallenge.constants;
+
+public enum TipoTransacao {
+    RECEITA, DESPESA
+}
--- src/main/java/com/github/ticoyk/mvchallenge/model/Transacao.java
+++ src/main/java/com/github/ticoyk/mvchallenge/model/Transacao.java
@@ -1,9 +1,16 @@
 package com.github.ticoyk.mvchallenge.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import com.github.ticoyk.mvchallenge.constants.TipoTransacao;
 
 @Entity
 public class Transacao {
@@ -11,6 +18,16 @@ public class Transacao {
     @GeneratedValue(strategy=GenerationType.IDENTITY)
     @Id
     private Long id;
+    
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoTransacao tipo;
 
+    @Column(nullable = false)
+    private Double valor;
+    
+    @Temporal(TemporalType.TIMESTAMP)
+    private java.util.Date createdDate;
+    
     Transacao(){}
 }
