5c5b1dc50e08d3e134902aa98170a4f2b08dc70a
Dídimo Yokoyama de Sousa <didimoykym@gmail.com>
2021-03-11 21:45:12 -0300

    Removido Import desnecessário

--- src/main/java/com/github/ticoyk/mvchallenge/model/Cliente.java
+++ src/main/java/com/github/ticoyk/mvchallenge/model/Cliente.java
@@ -10,10 +10,7 @@ import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
-import javax.persistence.Transient;
 import javax.validation.constraints.Size;
 
 import com.github.ticoyk.mvchallenge.constants.TipoCliente;
