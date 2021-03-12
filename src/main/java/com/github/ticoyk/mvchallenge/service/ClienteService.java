e41cc3d4cfd2ff8ebfb30e5c4e6630ebfa3c589c
Dídimo Yokoyama de Sousa <didimoykym@gmail.com>
2021-03-12 12:47:38 -0300

    Adicionado métodos para trazer todos e encontrar por id

--- src/main/java/com/github/ticoyk/mvchallenge/service/ClienteService.java
+++ src/main/java/com/github/ticoyk/mvchallenge/service/ClienteService.java
@@ -1,8 +1,12 @@
 package com.github.ticoyk.mvchallenge.service;
 
+import java.util.List;
+
 public interface ClienteService<T> {
 
     T registrarNovoCliente(T cliente);
     T atualizarCliente(T cliente);
-    
+    List<T> trazerTodosClientes();
+    T encontrarClientePorId(Long id);
+
 }
