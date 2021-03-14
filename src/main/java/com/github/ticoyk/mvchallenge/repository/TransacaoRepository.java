877758b8a1b87df804e573af134d968956dfdb0b
Dídimo Yokoyama de Sousa <didimoykym@gmail.com>
2021-03-14 13:57:08 -0300

    Criada Querys essenciais para criação de relatórios

--- src/main/java/com/github/ticoyk/mvchallenge/repository/TransacaoRepository.java
+++ src/main/java/com/github/ticoyk/mvchallenge/repository/TransacaoRepository.java
@@ -1,6 +1,6 @@
 package com.github.ticoyk.mvchallenge.repository;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.util.List;
 
 import com.github.ticoyk.mvchallenge.model.Transacao;
@@ -12,13 +12,51 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransacaoRepository extends CrudRepository<Transacao, Long>{
-    @Query(value="SELECT transacao FROM Transacao transacao where transacao.dataCriacao > subtrairData(:date, :days)")
-    List<Transacao> findTransacaoWithDayDifferenceByDay(@Param("date") LocalDateTime date, @Param("days")Integer days);
+    @Query(value="SELECT transacao FROM Transacao transacao WHERE transacao.dataCriacao > subtrairData(:date, :days)")
+    List<Transacao> findTransacaoWithDayDifferenceByDay(@Param("date") LocalDate date, @Param("days")Integer days);
+
+    @Query(value = 
+    "SELECT transacao FROM Cliente cliente " +
+    "INNER JOIN Conta conta ON cliente = conta.cliente " +
+    "INNER JOIN Transacao transacao ON conta = transacao.conta "+
+    "WHERE cliente.id = :clienteId AND transacao.dataCriacao BETWEEN :data1 AND :data2")
+    List<Transacao> findTransacaoByClientIdBetweenDates(
+        @Param("clienteId")Long clienteId, 
+        @Param("data1") LocalDate date1, 
+        @Param("data2") LocalDate data2);
+
+    @Query(value = 
+    "SELECT transacao FROM Cliente cliente " +
+    "INNER JOIN Conta conta ON cliente = conta.cliente " +
+    "INNER JOIN Transacao transacao ON conta = transacao.conta "+
+    "WHERE cliente.id = :clienteId")
+    List<Transacao> findTransacaoByClientId(
+        @Param("clienteId")Long clienteId);
+
+    @Query(value = 
+    "SELECT transacao FROM Cliente cliente " +
+    "INNER JOIN Conta conta ON cliente = conta.cliente " +
+    "INNER JOIN Transacao transacao ON conta = transacao.conta "+
+    "WHERE cliente.id = :clienteId AND transacao.dataCriacao < :data1")
+    List<Transacao> findTransacaoByClientIdFromStartToDate(
+        @Param("clienteId")Long clienteId, 
+        @Param("data1") LocalDate date1);
+
+    @Query(value = 
+    "SELECT sum(TRANSACAO.VALOR) FROM CLIENTE " +
+    "INNER JOIN CONTA ON CLIENTE.ID = CONTA.CLIENTE_ID " +
+    "INNER JOIN TRANSACAO ON CONTA.ID = TRANSACAO.CONTA_ID "+
+    "WHERE CLIENTE.ID = :clienteId AND " +
+    "TRANSACAO.IDENTIFICADOR = CLIENTE.ID", nativeQuery = true)
+    Double findByClienteValoPagoXPTO(@Param("clienteId") Long clienteId);
 
     @Query(value = 
     "SELECT count(TRANSACAO.ID) FROM CLIENTE " +
     "INNER JOIN CONTA ON CLIENTE.ID = CONTA.CLIENTE_ID " +
     "INNER JOIN TRANSACAO ON CONTA.ID = TRANSACAO.CONTA_ID "+
-    "WHERE CLIENTE.ID = :clienteId AND TRANSACAO.DATA_CRIACAO > subtrairData(:date, :days)", nativeQuery = true)
-    Integer findTransacaoCountByClienteAndDate(@Param("clienteId") Long clienteId, @Param("date") LocalDateTime date, @Param("days")Integer days);
+    "WHERE CLIENTE.ID = :clienteId AND " +
+    "TRANSACAO.DATA_CRIACAO > subtrairData(:date, :days) AND " +
+    "TRANSACAO.IDENTIFICADOR IS NULL", nativeQuery = true)
+    Integer findTransacaoCountByClienteAndDate(@Param("clienteId") Long clienteId, @Param("date") LocalDate date, @Param("days")Integer days);
+
 }
